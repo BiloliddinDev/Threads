64a42449c20c5bb0c144ace37781bfd4bd5b5d04
Biloliddin_dev <bilol9876@gmail.com>
2023-10-26 00:09:13 +0500

    add layaout section Components folder and you can Sucsesfuly

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+async function Page() {
+  return (
+    <main>
+      <h1 className="text-purple-600">Royhatdan otish</h1>
+    </main>
+  );
+}
+
+export default Page;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>BIlol</h1>
-    </main>
-  );
-}
