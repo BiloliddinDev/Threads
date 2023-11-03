5398d2fbc7a47b6b64d34f0fbb87db0cab788719
Biloliddin_dev <bilol9876@gmail.com>
2023-11-03 22:27:26 +0500

    add new section and frist  bacnet

--- Components/forms/AccountProfile.tsx
+++ Components/forms/AccountProfile.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { useForm } from "react-hook-form";
 import {
   Form,
@@ -18,6 +18,8 @@ import { UserValidation } from "@/lib/validations/user";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { Textarea } from "@/components/ui/textarea";
+import { isBase64Image } from "@/lib/utils";
+// import { useUploadThing } from "@/lib/uploadthing";
 
 interface Props {
   user: {
@@ -32,25 +34,48 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
-      profile_photo: "",
-      name: "",
-      username: "",
-      bio: "",
+      profile_photo: user?.image || "i",
+      name: user?.name || "",
+      username: user?.userName || "",
+      bio: user.bio || "",
     },
   });
 
   function onSubmit(values: z.infer<typeof UserValidation>) {
-    console.log(values);
+    const blob = values.profile_photo;
+
+    const hasImageChanged = isBase64Image(blob);
+    if (hasImageChanged) {
+      const imgRes = 
+      
+    }
   }
 
   const handleImage = (
-    e: ChangeEvent,
+    e: ChangeEvent<HTMLInputElement>,
     fieldChange: (value: string) => void
   ) => {
     e.preventDefault();
+    const fileReader = new FileReader();
+
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      setFiles(Array.from(e.target.files));
+
+      if (!file.type.includes("image")) return;
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || "";
+        fieldChange(imageDataUrl);
+      };
+
+      fileReader.readAsDataURL(file);
+    }
   };
 
   return (
